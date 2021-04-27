28d183c5b839300c09509075271952bcb50e4d69
Cryptocode <crytocode99@gmail.com>
2021-04-27 08:01:36 +0700

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -4,10 +4,10 @@ export const baseColors = {
   failure: "#ED4B9E",
   primary: "#ff9933",
   primaryBright: "#e55b00",
-  primaryDark: "#ff7519",
+  primaryDark: "#1D19FF",
   secondary: "#7645D9",
   success: "#31D0AA",
-  warning: "#FFB237",
+  warning: "#373AFF",
 };
 
 export const brandColors = {
@@ -40,7 +40,7 @@ export const darkColors: Colors = {
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
-  invertedContrast: "#191326",
+  invertedContrast: "#2A2A82",
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
@@ -48,7 +48,7 @@ export const darkColors: Colors = {
   textDisabled: "#666171",
   textSubtle: "#c9c4d4",
   borderColor: "#524B63",
-  card: "#27262c",
+  card: "#040248",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },
